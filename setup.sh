43428428e54b6eb99e64969db09f4c23d65a0347
Tim Thirion <t.a.thirion@gmail.com>
2013-07-04 15:45:07 -0400

    Symlink .ghc folder

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ echo "Deleting old files"
 rm ~/.vimrc
 rm -rf ~/.vim
 rm ~/.zshrc
+rm -rf ~/.ghc
 rm ~/.osx
 rm ~/.gitconfig
 rm ~/.gitignore_global
@@ -10,6 +11,7 @@ echo "Copying files"
 ln -sv ~/dotfiles/vimrc ~/.vimrc
 ln -sv ~/dotfiles/vim ~/.vim
 ln -sv ~/dotfiles/zshrc ~/.zshrc
+ln -sv ~/dotfiles/ghc ~/.ghc
 ln -sv ~/dotfiles/osx ~/.osx
 ln -sv ~/dotfiles/gitconfig ~/.gitconfig
 ln -sv ~/dotfiles/gitignore_global ~/.gitignore_global
