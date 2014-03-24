eec479b5c50bb8c28dc30716e1f4eb2c6037b6ed
Tim Thirion <t.a.thirion@gmail.com>
2014-03-24 17:41:33 -0400

    Small changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 echo "Deleting old files"
-rm ~/.vimrc
+rm -f ~/.vimrc
 rm -rf ~/.vim
-rm ~/.zshrc
-rm ~/.config/fish/config.fish
-rm ~/.tmux.conf
+rm -f ~/.zshrc
+rm -f ~/.config/fish/config.fish
+rm -f ~/.tmux.conf
 rm -rf ~/.ghc
-rm ~/.osx
-rm ~/.gitconfig
-rm ~/.gitignore_global
+rm -f ~/.gitconfig
+rm -f ~/.gitignore_global
 
 echo "Copying files"
 ln -sv ~/dotfiles/vimrc ~/.vimrc
@@ -18,7 +17,6 @@ ln -sv ~/dotfiles/zshrc ~/.zshrc
 ln -sv ~/dotfiles/config.fish ~/.config/fish/config.fish
 ln -sv ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sv ~/dotfiles/ghc ~/.ghc
-ln -sv ~/dotfiles/osx ~/.osx
 ln -sv ~/dotfiles/gitconfig ~/.gitconfig
 ln -sv ~/dotfiles/gitignore_global ~/.gitignore_global
 
@@ -26,8 +24,8 @@ git config --global core.excludesfile ~/.gitignore_global
 
 if [[ "$(uname)" == "Darwin" ]]; then
     echo "Setting OS X defaults"
-    chmod +x ~/.osx
-    /bin/bash ~/.osx
+    chmod +x ./osx
+    /bin/bash ./osx
 fi
 
 echo "Done"
