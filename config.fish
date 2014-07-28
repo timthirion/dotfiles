362ac471ac938f96669af51dcf202e4359a5bb90
Tim Thirion <t.a.thirion@gmail.com>
2014-07-28 11:06:47 -0400

    Add git branch status to fish shell; enable mouse mode in tmux

--- config.fish
+++ config.fish
@@ -46,6 +46,28 @@ end;
 function colors; /bin/bash colors.sh; end;
 function rot13; tr "a-zA-Z" "n-za-mN-ZA-M" $argv; end;
 
+set fish_git_dirty_color red
+set fish_git_not_dirty_color green
+
+function parse_git_branch
+  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+  set -l git_diff (git diff)
+
+  if test -n "$git_diff"
+    echo (set_color $fish_git_dirty_color)$branch(set_color normal)
+  else
+    echo (set_color $fish_git_not_dirty_color)$branch(set_color normal)
+  end
+end
+
+function fish_prompt
+  if test -d .git
+    printf '%s@%s %s%s%s:%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
+  else
+    printf '%s@%s %s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+  end
+end
+
 set VTK ~/Kitware/VTK
 set PARAVIEW ~/Kitware/ParaView
 
