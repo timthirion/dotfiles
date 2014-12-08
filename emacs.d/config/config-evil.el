621e111afdd510da36cc50f69daf8ab0fca5cc10
Tim Thirion <t.a.thirion@gmail.com>
2014-12-08 16:01:27 -0500

    Better evil config

--- emacs.d/config/config-evil.el
+++ emacs.d/config/config-evil.el
@@ -1,3 +1,8 @@
+; Basic options
+(setq evil-auto-indent t)
+(setq evil-regexp-search t)
+(setq evil-want-C-i-jump t)
+
 ; The chord 'jk' returns us to normal mode. 
 (key-chord-define evil-insert-state-map (kbd "jk") 'evil-normal-state)
 
@@ -9,8 +14,21 @@
 (setq evil-replace-state-cursor '("red" bar))
 (setq evil-operator-state-cursor '("red" hollow))
 
-; Disable arrow keys
-(global-unset-key (kbd "<left>"))
-(global-unset-key (kbd "<up>"))
-(global-unset-key (kbd "<right>"))
-(global-unset-key (kbd "<down>"))
+; Arrow keys move through frames in normal mode
+(define-key evil-normal-state-map (kbd "<left>") 'windmove-left)
+(define-key evil-normal-state-map (kbd "<up>") 'windmove-up)
+(define-key evil-normal-state-map (kbd "<right>") 'windmove-right)
+(define-key evil-normal-state-map (kbd "<down>") 'windmove-down)
+
+; Disable arrow keys when in insert mode
+(define-key evil-insert-state-map (kbd "<left>") nil)
+(define-key evil-insert-state-map (kbd "<up>") nil)
+(define-key evil-insert-state-map (kbd "<right>") nil)
+(define-key evil-insert-state-map (kbd "<down>") nil)
+
+; Disable arrow keys when in visual mode as well
+(define-key evil-visual-state-map (kbd "<left>") nil)
+(define-key evil-visual-state-map (kbd "<up>") nil)
+(define-key evil-visual-state-map (kbd "<right>") nil)
+(define-key evil-visual-state-map (kbd "<down>") nil)
+
