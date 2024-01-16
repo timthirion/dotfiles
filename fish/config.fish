fe44984d1765fb51c759d1d3014e2726ab12def9
Tim Thirion <t.a.thirion@gmail.com>
2024-01-16 11:23:42 -0500

    Add Ruby setup to Fish

--- fish/config.fish
+++ fish/config.fish
@@ -30,6 +30,11 @@ set -gx PATH                                 \
 
 set -gx Qt5_DIR /usr/local/Cellar/qt/5.12.3
 
+# Ruby
+source (brew --prefix)/Cellar/chruby-fish/1.0.0/share/fish/vendor_functions.d/chruby.fish
+source (brew --prefix)/Cellar/chruby-fish/1.0.0/share/fish/vendor_conf.d/chruby_auto.fish
+chruby ruby-3.1.3
+
 # Determine number of processor cores available
 switch (uname)
   case Darwin
