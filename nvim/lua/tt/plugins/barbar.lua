a67ed8bc5d5ce3fb83b5ebbd455787072439fb71
Tim Thirion <t.a.thirion@gmail.com>
2024-02-03 18:20:07 -0500

    Disable barbar for now in Neovim

--- nvim/lua/tt/plugins/barbar.lua
+++ nvim/lua/tt/plugins/barbar.lua
@@ -1,6 +1,7 @@
 return {
   {
     'romgrk/barbar.nvim',
+    enabled = false,
     dependencies = {
       'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
